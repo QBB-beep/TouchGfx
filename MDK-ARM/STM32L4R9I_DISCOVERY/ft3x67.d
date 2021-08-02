36d8ab8ddd7bdecb7118af139dea4ead8e5bb6e0
QBB-beep <1956337278@qq.com>
2021-08-03 00:23:17 +0800

    first commit

---
+++ MDK-ARM/STM32L4R9I_DISCOVERY/ft3x67.d
@@ -0,0 +1,4 @@
+stm32l4r9i_discovery\ft3x67.o: ../Drivers/BSP/Components/ft3x67/ft3x67.c
+stm32l4r9i_discovery\ft3x67.o: ../Drivers/BSP/Components/ft3x67/ft3x67.h
+stm32l4r9i_discovery\ft3x67.o: ../Drivers/BSP/Components/ft3x67/../Common/ts.h
+stm32l4r9i_discovery\ft3x67.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
